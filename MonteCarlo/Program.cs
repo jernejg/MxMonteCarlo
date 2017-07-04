8d8789ad05a4441ca2a267d4f37e0280082b84f1
Jernej Goricki <Jernej.Goricki@mishcon.com>
2017-07-04 22:09:03 +0100

    initial commit

--- MonteCarlo/Program.cs
+++ MonteCarlo/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MonteCarlo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            const int numberOfTimesWaitingForABus = 10000000;
+            var waitingTime = 0;
+            Parallel.For(0, numberOfTimesWaitingForABus + 1, i =>
+            {
+                //Waiting for A between 0 and 12 minutes
+                var busAArrivesIn = StaticRandom.Rand(0, 13);
+                //Waiting for B between 0 and 8 minute
+                var busBArrivesIn = StaticRandom.Rand(0, 9);
+                //Take the first bus (thread safe incrementation)
+                Interlocked.Add(ref waitingTime, Math.Min(busAArrivesIn, busBArrivesIn));
+            });
+
+            var averageWaitingTime = (float)waitingTime / numberOfTimesWaitingForABus;
+
+            Console.WriteLine(averageWaitingTime);
         }
     }
 }
---
+++ MonteCarlo/StaticRandom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace MonteCarlo
+{
+    /// <summary>
+    /// Give each thread its own seed value
+    /// </summary>
+    public static class StaticRandom
+    {
+        private static int _seed = Environment.TickCount;
+
+        private static readonly ThreadLocal<Random> Random =
+            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref _seed)));
+
+        public static int Rand(int a, int b)
+        {
+            return Random.Value.Next(a, b);
+        }
+    }
+}
